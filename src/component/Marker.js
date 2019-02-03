5ca4f0a4fbf347619c8b1b34f64ad80e106d5435
David Herr <davidherraz@outlook.com>
2019-02-03 00:16:02 -0700

    Comment initial marker functionality.

--- src/component/Marker.js
+++ src/component/Marker.js
@@ -4,17 +4,20 @@ import { Marker } from 'react-google-maps';
 
 class Markers extends React.Component {
   static propTypes = {
-    locations: PropTypes.array.isRequired,
+    locations: PropTypes.object.isRequired,
   };
 
   render() {
+    // set locations object to passed props
     const { locations } = this.props;
+    // further deconstruct locations object for map function
     const mapLocations = locations.locations;
 
     return (
       <>
         {mapLocations.map((location) => (
           <Marker
+            key={(location.position.lat, location.position.lng)}
             title={location.title}
             name={location.title}
             position={location.position}
