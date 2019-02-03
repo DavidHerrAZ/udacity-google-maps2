ce8ea607977d144e0c539ba2e928b0c2a8d50a49
David Herr <davidherraz@outlook.com>
2019-02-02 22:19:29 -0700

    Add locations state for 5 required locations.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 import Map from './component/Map';
+import locations from './data/locations.json';
 
 class App extends Component {
+  state = {
+    locations: [],
+  };
+
+  // Set state to required 5 app locations on app mount
+  componentDidMount() {
+    this.setState({ locations });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <Map />
+        {/* Pass locations state for use in initial marker drops */}
+        <Map locations={this.state.locations} />
       </div>
     );
   }
