a7354a6dc2058efd1be906baca4793e4bb12a509
David Herr <davidherraz@outlook.com>
2019-02-04 18:01:57 -0700

    Add basic search bar functionality with query state.

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <main>
-          <Sidebar className="App-sidebar" />
-          <Map className="App-map" locations={this.state.locations} />
+          <Sidebar className="App-sidebar" locations={this.state.locations} />
+          <Map locations={this.state.locations} />
         </main>
       </div>
     );
--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
 class Sidebar extends React.Component {
+  state = {
+    query: '',
+    searchResults: [],
+  };
+
+  updateQuery = (query) => {
+    this.setState({ query: query }, this.searchLocations(query));
+  };
+
+  clearQuery = () => {
+    this.setState({ query: '' });
+  };
+
+  searchLocations = (query) => {
+    console.log(query);
+  };
+
   render() {
+    const { query, searchResults } = this.state;
+    const { locations } = this.props;
+
     return (
       <div className="App-sidebar">
-        <input />
+        <input
+          className="App-search"
+          type="text"
+          placeholder="Search through locations..."
+          value={query}
+          onChange={(event) => {
+            this.updateQuery(event.target.value);
+          }}
+        />
       </div>
     );
   }
